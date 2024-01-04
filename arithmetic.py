79dab563f06ebce88ebfc8714c4bd3c49792dd45
Magdielian-code <oziomagaecheta81@gmail.com>
2024-01-04 12:04:40 +0100

    added arithmetic.oy file to test arithmetical functions.

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    """Add function"""
+    return a + b
+
+def subtract(a,b):
+    """Subtrat function"""
+    return a - b
+
+def multiply(a,b):
+    """Multiply function"""
+    return a*b
+
+def divide(a,b):
+    """Divide function"""
+    return a/b
